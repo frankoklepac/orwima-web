1e29737e970a3bce93acb80d5c4455f2c631cfd9
FrankoKlepac <itsmefranko1@gmail.com>
2024-01-10 17:15:06 +0100

    Added JavaScript script

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+
+var modal = document.getElementById("abilityModal");
+
+
+var abilities = document.querySelectorAll(".ability img");
+
+abilities.forEach(function(ability) {
+  ability.addEventListener("click", function() {
+    // Get the ability information
+    var iconSrc = this.src;
+    var abilityName = this.alt;
+    var abilityDescription = this.dataset.description; 
+
+    document.getElementById("abilityIcon").src = iconSrc;
+    document.getElementById("abilityName").innerText = abilityName;
+    document.getElementById("abilityDescription").innerText = abilityDescription;
+
+    modal.style.display = "block";
+  });
+});
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
